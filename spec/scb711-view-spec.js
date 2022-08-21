cb3d342cb4053dbfee3e4673c921a9410978402b
rnikrozoft <rnikrozoft@gmail.com>
2022-08-21 14:35:34 +0700

    first commit

---
+++ spec/scb711-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGifCardsView from '../lib/scb711-view';
+
+describe('AtomGifCardsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
